b474dad1f903de97ee51f6a16c433d8bdab346ff
Samruddhi <108574708+Samruddhisuryawanshi5@users.noreply.github.com>
2025-11-20 11:47:27 +0530

    new playwright changees

---
+++ tests/test.spec.ts
@@ -0,0 +1,13 @@
+import{test,expect} from '@playwright/test';
+
+test.only('open google website',async({page})=>{
+
+   await page.goto("https://www.google.com/");
+   await page.locator("textarea[id='APjFqb']").fill("youtube");
+   await page.locator("textarea[id='APjFqb']").press("Enter");
+   
+
+
+
+
+});
